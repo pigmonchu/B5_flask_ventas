9d4a4c111bdffb652899341e62f69914ffc7485e
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-05-26 23:01:53 +0200

    header footer

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    fVentas = open('./sales10.csv', 'r')
+    fVentas = open('./sales.csv', 'r')
     csvreader = csv.reader(fVentas, delimiter=',')
 
     registros = []
@@ -20,4 +20,4 @@ def index():
                 d[linea[0]] = {'ingresos': float(linea[11]), 'beneficios': float(linea[13])}
     
 
-    return render_template('region.html')
+    return render_template('region.html',ventas=d)
