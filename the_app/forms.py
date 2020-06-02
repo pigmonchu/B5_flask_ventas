2b515e95e637d66eb0fbf7ac145d2213382704ac
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2020-06-02 23:06:35 +0200

    Validación a medida

--- the_app/forms.py
+++ the_app/forms.py
@@ -1,11 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
 
+def valida_coste(form, field):
+    if field.data > form.precio_unitario.data:
+        raise ValidationError('Ya te he dicho que el coste unitario ha de ser menor que el precio.')
 
 class ProductForm(FlaskForm):
     tipo_producto = StringField('Tipo de Producto', validators=[DataRequired(), Length(min=3, message="Debe tener al menos tres caracteres")])
     precio_unitario = FloatField('Precio U.', validators=[DataRequired(message="Introduce algo, nano")])
-    coste_unitario = FloatField('Coste U.', validators=[DataRequired()])
+    coste_unitario = FloatField('Coste U.', validators=[DataRequired(), valida_coste])
 
     submit = SubmitField('Aceptar')
+
+    '''
+    def validate_coste_unitario(self, field):
+        if field.data > self.precio_unitario.data:
+            raise ValidationError('El coste unitario ha de ser menor o igual que el precio ')
+    '''
